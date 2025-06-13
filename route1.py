9b34b1cb81bb058515880634f649877182107e1a
joaogt01 <joao.dev06@gmail.com>
2025-06-12 21:42:07 -0300

    beta do jogo

--- route1.py
+++ route1.py
@@ -1,5 +1,12 @@
 import random
 import time
+def menu_pokedex():
+    print('''Digite
+1 para Listar Detalhes
+2 para Apagar Registro
+0 para voltar ao menu principal
+Escolha uma ação:''')
+
 def menu():
     print("--------------------")
     print('''9 - Para abrir esse menu
@@ -13,13 +20,28 @@ def menu():
 def grama(a):
     if a == "G":
         pokemon_selvagem = random.randint(0,1)
+        capturado = {'pokemon': random.choice(pokemons), 'atributos': atributo()}
         if pokemon_selvagem == 1:
             print("Um pokemon selvagem apareceu!\nCapturar ou correr? [1-Capturar ou 2-Correr]")
             cap_correr = int(input())
             if cap_correr == 1:
-                pokedex.append(random.choice(pokemons))
-
-
+                if capturado not in pokedex:
+                    pokedex.append(capturado)
+                    print("Pokemon Capturado!")
+                    
+            elif cap_correr == 2:
+                print("Fujão!")
+def atributo():
+    lista_atributos = []
+    for i in range(6):
+        lista_atributos.append(random.randint(0,100))
+    hp = "HP",lista_atributos[0]
+    atk = "Atk",lista_atributos[1]
+    deff = "Def",lista_atributos[2]
+    sp_atk = "Sp atk",lista_atributos[3]
+    sp_def = "Sp def",lista_atributos[4]
+    speed = "Speed",lista_atributos[5]
+    return hp,atk,deff,sp_atk,sp_def,speed
 l = 19
 c = 6
 campo = [['A','A','A','A','A',' ',' ','A','A','A','A','A'],
@@ -43,8 +65,10 @@ campo = [['A','A','A','A','A',' ',' ','A','A','A','A','A'],
         ['A','G','G','G',' ',' ',' ','G','G','A','A','A'],
         ['A','A','A','A','A','A','G','A','A','A','A','A']]
 
-pokemons = [ 'Ratata', 'Pidgey', 'Weedle', 'Caterpie', 'Paras', 'Charmander', 'Bulbasaur', 'Squirtle', 'Pikachu' , 'Evee' ]
+pokemons = [ 'ratata', 'pidgey', 'weedle', 'caterpie', 'paras', 'charmander', 'bulbasaur', 'squirtle', 'pikachu' , 'evee' ]
 pokedex = []
+
+
 jogador = campo[19][6]
 print("entrando na rota 1")
 time.sleep(3)
@@ -60,6 +84,14 @@ while True:
         if jogador == "A":
             print("Bump!")
             l += 1
+        elif jogador == "G":
+            grama(jogador)
+        elif l < 0 or l > 19:
+            print("Fim de jogo")
+            break
+        elif jogador == "E" and campo[l + 1][c] != "E":
+            print("Bump!")
+            l += 1
         else:
             print(jogador)
         print("-----")
@@ -69,6 +101,11 @@ while True:
         if jogador == "A":
             print("Bump!")
             l -= 1
+        elif jogador == "G":
+            grama(jogador)
+        elif l < 0 or l > 19:
+            print("Fim de jogo")
+            break
         else:
             print(jogador)
             print("-----")
@@ -78,6 +115,8 @@ while True:
         if jogador == "A":
             print("Bump!")
             c+=1
+        elif jogador == "G":
+            grama(jogador)
         else:
             print(jogador)
         print("-----")
@@ -87,11 +126,31 @@ while True:
         if jogador == "A":
             print("Bump!")
             c-=1
+        elif jogador == "G":
+            grama(jogador)
         else:
             print(jogador)
         print("-----")
     elif entrada == 5:
-        print("5")
+        print("Pokémons capturados:")
+        for p in pokedex:
+            print(f"- {p['pokemon']}")
+        while True:
+            menu_pokedex()
+            opcao_pokedex = int(input())
+            if opcao_pokedex == 0:
+                break
+            elif opcao_pokedex == 1:
+                consultar_atributo = input("Digite o pokemon a ser consultado:").lower()
+                for registro in pokedex:
+                    if registro['pokemon'] == consultar_atributo:
+                        print(registro['atributos'])
+            elif opcao_pokedex == 2:
+                consultar_atributo = input("Digite o pokemon a ser apagado:").lower()
+                for registro in pokedex:
+                    if registro['pokemon'] == consultar_atributo:
+                        pokedex = [p for p in pokedex if p['pokemon'] != consultar_atributo]
+
     elif entrada == 0:
         break
     else:
